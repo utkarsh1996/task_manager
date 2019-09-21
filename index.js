7f54624f91f9f7012083876949c89663433332ef
Shaurya <ShauryaMathur>
2019-09-21 18:03:18 +0530

    Attempt 8

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ console.log(__dirname);
 //Serve static assets if in production
 if(process.env.NODE_ENV==='production'){
   //Set a static folder
-  app.use(express.static(__dirname+'dist/scrum-ui'));
+  app.use(express.static('dist/scrum-ui'));
 
   app.get('*',(req,res)=>{
       res.sendFile(path.resolve(__dirname,'dist','scrum-ui','index.html'));
